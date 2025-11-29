65fe22e70d988d62d272bd945bcba138e39e0aa1
thonedra-dev <thonedra.dev@gmail.com>
2025-11-29 22:40:45 +0800

    Array One-D, Two_D slicing

---
+++ day_one_three.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+arr1 = np.array([10,20,30,40,50,60,70,80])
+arr2 = np.array([[1,2,3],
+                 [4,5,6],
+                 [7,8,9]])
+
+# ONE-D slicing and TWO-D slicing.
+
+print(arr1[1:7])
+print(arr2[0:3, 0:3])  
+
+
+
