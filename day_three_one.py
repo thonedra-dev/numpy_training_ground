75ab6631be68ae3bb355df32d3da65c65c961a26
thonedra-dev <thonedra.dev@gmail.com>
2025-12-01 16:09:27 +0800

    understanind the axis ( 0, 1, 2)

--- day_three_one.py
+++ day_three_one.py
@@ -11,3 +11,15 @@ print("Original Array:\n", arr, "\n")
 
 print("Sum of all elements in the array : ", np.sum(arr))
 print("Largest of all elements in the array : ", np.max(arr))
+print("The average value within the array: ", np.mean(arr))
+print()
+print()
+
+print(np.sum(arr, axis=1)) # This will add the rows in a single sheet.
+print(np.sum(arr, axis=2)) # This will add the cols in a single row.
+print(np.sum(arr, axis=0)) # This will add every rows and cols from the different sheets into a single sheet.
+# The concept behind is that, 
+# sheet => row => col,
+# So, rows in a sheet
+# So, cols in a row
+# So, rows and cols of a sheet.
